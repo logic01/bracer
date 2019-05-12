f89ad09edbcf148a065c63bfe7262033c63738b5
Shane Chambers <shanechambers85@gmail.com>
2019-05-12 13:20:38 -0600

    Created a Temp endpoint that will allow me to save the word doc into the document table. Then created the download endpoint that will actually be used. Added the address create to the sql script

--- dotnet/PhysiciansReach/PhysiciansReach/Controllers/DocumentController.cs
+++ dotnet/PhysiciansReach/PhysiciansReach/Controllers/DocumentController.cs
@@ -45,7 +45,7 @@ namespace PhysiciansReach.Controllers
         [HttpPut("Document/{documentId}")]
         public ActionResult<DocumentModel> Put(int documentId, [FromBody] DocumentModel document)
         {
-            _logging.Log(LogSeverity.Info, "Pu Document");
+            _logging.Log(LogSeverity.Info, "Put Document");
 
             if (document.DocumentId != documentId)
             {
@@ -55,15 +55,51 @@ namespace PhysiciansReach.Controllers
             return _business.Update(document);
         }
 
-        [HttpGet("Document/{patientId}/Download")]
+        [HttpGet("Document/{patientId}/TestCreateByPatientId")]
         public FileResult GetWordByPatient(int patientId)
         {
-            FileResult fr = new FileContentResult(_business.GetDocByPatient(patientId), "application/vnd.ms-word")
+            //This method will be dying once I can figure out how the inserts, and updates are working for 
+            //intake forms works. Doing this by patient allows me to gather all of the intake forms and
+            //create one massive form. I am thinking there might actually be 3 seperate forms, but I don't quite understand that
+            //
+            //To use this method you will need to run the script that creates all of the intake forms, then run the patient id
+            //through this end point. This will save the newly created document. Then you can call the other endpoint.
+            _logging.Log(LogSeverity.Info, $"Temp Endpoint to save the doc by patient id {patientId}");
+
+            var temp = new DocumentModel()
+            {
+                PhysicianId = 24,
+                IntakeFormId = 1,
+                Type = DocumentType.IntakeForm,
+                Content = _business.GetDocByPatient(patientId)
+            };
+            var doc = _business.Create(temp);
+
+            FileResult fr = new FileContentResult(doc.Content, "application/vnd.ms-word")
             {
                 FileDownloadName = string.Format("Exam_{0}_{1}.docx", DateTime.Now.ToString("yyMMdd"), "Doc")
             };
             return fr;
         }
 
+        /// <summary>
+        /// This will pull the content of the Document specifically then formats it to a word document for
+        /// downloading.
+        /// </summary>
+        /// <param name="documentId"></param>
+        /// <returns></returns>
+        [HttpGet("Document/{documentId}/Download")]
+        public FileResult GetWordByDocumentId(int documentId)
+        {
+            _logging.Log(LogSeverity.Info, $"Get Document {documentId}");
+            var document = _business.Get(documentId);
+            if (document == null)
+                throw new Exception("Document Not Found");
+            FileResult fr = new FileContentResult(document.Content, "application/vnd.ms-word")
+            {
+                FileDownloadName = string.Format("Exam_{0}_{1}.docx", DateTime.Now.ToString("yyMMdd"), "Doc")
+            };
+            return fr;
+        }
     }
 }
